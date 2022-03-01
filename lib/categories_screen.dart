23d4510a1e69749c22fcc3a4eb4419239be48aab
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2022-03-01 15:07:14 +0100

    added home screen with categories

---
+++ lib/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals_fame/category_item.dart';
+import './dummy_categories.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Meals Fame'),
+        ),
+        body: GridView(
+          padding: const EdgeInsets.all(15),
+          children: dummycategories.map((categoryData) {
+            return CategoryItem(
+                title: categoryData.title, color: categoryData.color);
+          }).toList(),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              childAspectRatio: 3 / 2),
+        ));
+  }
+}
---
+++ lib/category_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  // ignore: prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+  CategoryItem({required this.title, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      // color: color,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: <Color>[color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
